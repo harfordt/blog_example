f2fd754f52bf07a5a30cbf12597a92038ce6dd71
harfordt <tim.harford@wgc.school.nz>
2019-02-08 15:29:13 +1300

    Initial commit

--- app.py
+++ app.py
@@ -1,6 +1,27 @@
+# these first 2 lines are only for on the school system
+import sys
+
+sys.path.append('N:\python-modules')
+
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
 
 
+@app.route('/')
+def hello_world():
+    return render_template("home.html")
 
-print("hi")
 
-print("world")
+@app.route('/profile')
+def profile():
+    return render_template("profile.html")
+
+
+@app.route('/gallery')
+def gallery_page():
+    return render_template("gallery.html")
+
+
+if __name__ == "__main__":
+    app.run()
