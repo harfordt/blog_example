b9cb13d56988558190ae0c9de7077ceb83fc9a6f
harfordt <tim.harford@wgc.school.nz>
2019-02-12 22:57:19 +1300

    Added code to connect to/create db, and add tables

--- app.py
+++ app.py
@@ -7,9 +7,38 @@ from flask import Flask, render_template, request, redirect
 import sqlite3
 from sqlite3 import Error
 
-
+DATABASE_NAME = "blog.db"
 app = Flask(__name__)
 
+
+def create_connection(db_file):
+    """create a connection to the sqlite db"""
+    try:
+        connection = sqlite3.connect(db_file)
+        initialise_tables(connection)
+        return connection
+    except Error as e:
+        print(e)
+
+    return None
+
+
+def create_table(con, query):
+    if con is not None:
+        try:
+            c = con.cursor()
+            c.execute(query)
+        except Error as e:
+            print(e)
+
+
+def initialise_tables(con):
+    create_blog_table = """CREATE TABLE IF NOT EXISTS entry(
+                            id integer PRIMARY KEY,
+                            title text NOT NULL,
+                            body text NOT NULL
+                            )"""
+    create_table(con, create_blog_table)
 
 
 @app.route('/')
@@ -26,12 +55,10 @@ def profile():
 def gallery_page():
     return render_template("gallery.html")
 
+
 @app.route('/new-blog-post')
 def new_blog_post():
-    return render_template("new_blog_post.html")
-
-
-
+    return render_template("new-blog-post.html")
 
 
 @app.route('/do-insert-blog-post', methods=['POST'])
@@ -41,9 +68,15 @@ def do_insert_blog_post():
     print(title)
     print(body)
 
+    con = create_connection(DATABASE_NAME)
+    post = (title, body)
+    sql = """INSERT INTO entry(title, body) VALUES (?,?);"""
+    cur = con.cursor()
+    cur.execute(sql, post)
+    con.commit()
+    con.close()
+
     return redirect('/')
-
-
 
 
 if __name__ == "__main__":
